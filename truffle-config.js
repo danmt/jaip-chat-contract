25ce5200950bdc54e2f323ce71f67e6ee039ce36
Andres <andresmgsl@gmail.com>
2023-07-09 01:05:25 +0100

    feat: deploy added

--- truffle-config.js
+++ truffle-config.js
@@ -41,27 +41,30 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { INFURA_API_KEY_GOERLI, INFURA_API_KEY_SEPOLIA, INFURA_API_KEY_MAINNET, MNEMONIC } = process.env;
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a managed Ganache instance for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY_SEPOLIA),
+      network_id: "11155111",
+      gas: 4465030,
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY_GOERLI),
+      network_id: '5',
+      gas: 4465030
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY_MAINNET),
+      network_id: '1'
     }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
